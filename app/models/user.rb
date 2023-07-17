d8ff496b9366fb1afbcdaeb336b3bfe96f08e369
alexanderpierce <takaaki.110706@gmail.com>
2023-07-17 17:24:37 +0900

    Add gem annotate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  age        :integer          not null
+#  email      :string           not null
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class User < ApplicationRecord
   has_many :tweets
 
--- test/models/tweet_test.rb
+++ test/models/tweet_test.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: tweets
+#
+#  id         :bigint           not null, primary key
+#  content    :string
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint
+#
+# Indexes
+#
+#  index_tweets_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 require "test_helper"
 
 class TweetTest < ActiveSupport::TestCase
