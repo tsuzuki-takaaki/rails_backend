6f93278af8caa020e3470ae52f8852c0acf000b0
alexanderpierce <takaaki.110706@gmail.com>
2024-02-02 16:35:29 +0900

    Recreate environment

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  age        :integer          not null
+#  email      :string           not null
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 RSpec.describe User, type: :model do
   # Remove RSpec/EmptyExampleGroup of .rubocop_todo.yml after add some specs
 end
